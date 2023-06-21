a4d1ab5dedcdafc77945a3a0f105f857ca81919f
Andrew_demo <andrew.lestrange@puppet.com>
2023-06-21 16:08:24 +1000

    Adding log rotation params

--- spec/classes/telegraf_spec.rb
+++ spec/classes/telegraf_spec.rb
@@ -36,6 +36,9 @@ describe 'telegraf' do
           interval: '60s',
           metric_batch_size: '1000',
           metric_buffer_limit: '10000',
+          logfile_rotation_interval: '0',
+          logfile_rotation_max_size: '0',
+          logfile_rotation_max_archives: 0,
           flush_interval: '60s',
           global_tags: {
             'dc' => 'dc',
